602e8eb8b4558e87c9b408b47884907835ef3ff9
ThisIsProx <101939838+ThisIsProx@users.noreply.github.com>
2022-04-09 17:51:30 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Site1</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400&display=swap" rel="stylesheet">
+</head>
+<body>
+<section class="sub_block new_dex">
+    <form id="subscription_form" class="form_sub">
+        <h1>Subscribe</h1>
+        <p class="title">Sign up with your email address to receive news and updates.</p>
+        <div class="inputs">
+            <label>
+                <input type="text" name="name" required placeholder="First Name">
+            </label>
+            <label>
+                <input type="text" name="name" required placeholder="Last Name">
+            </label>
+            <label>
+                <input type="email" name="email" required placeholder="Email Address">
+                <button type="submit">SIGN UP</button>
+            </label>
+        </div>
+    </form>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="main.js"></script>
+</section>
+</body>
+</html>
